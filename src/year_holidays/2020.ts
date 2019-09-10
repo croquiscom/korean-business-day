3aef44136627115bc859dfa88b48fe9cbb222456
Jinho Hyeon <neverlish@gmail.com>
2019-09-10 12:33:43 +0900

    연도별 공휴일을 별도 파일로 분리

---
+++ src/year_holidays/2020.ts
@@ -0,0 +1,49 @@
+import { IYearHolidays } from '.';
+
+const HOLIDAYS_2020: IYearHolidays = {
+  1: {
+    1: true, // 신정
+    24: true, // 설날 연휴
+    25: true, // 설날
+    26: true, // 설날 연휴
+    27: true, // 대체 휴일
+  },
+  2: {
+
+  },
+  3: {
+    1: true, // 삼일절
+  },
+  4: {
+    15: true, // 21대 국회의원 선거일
+    30: true, // 부처님 오신날
+  },
+  5: {
+    5: true, // 어린이날
+  },
+  6: {
+    6: true, // 현충일
+  },
+  7: {
+
+  },
+  8: {
+    15: true, // 광복절
+  },
+  9: {
+    30: true, // 추석 연휴
+  },
+  10: {
+    1: true, // 추석
+    2: true, // 추석 연휴
+    3: true, // 개천절
+    9: true, // 한글날
+  },
+  11: {
+  },
+  12: {
+    25: true, // 성탄절
+  },
+};
+
+export default HOLIDAYS_2020;
---
+++ src/year_holidays/index.ts
@@ -0,0 +1,12 @@
+import HOLIDAYS_2019 from './2019';
+import HOLIDAYS_2020 from './2020';
+
+interface IYearHolidays {
+  [month: number]: { [day: number]: boolean };
+}
+
+export {
+  IYearHolidays,
+  HOLIDAYS_2019,
+  HOLIDAYS_2020,
+};
