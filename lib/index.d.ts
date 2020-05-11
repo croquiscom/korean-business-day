7d2295f75c77502e3252e544fdcf2e1bf33f2463
holynls <holynls@gmail.com>
2020-05-11 15:13:42 +0900

    isHoliday 함수 추가

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
+export declare function isHoliday(date: Date): boolean;
 export declare function getNextKoreanBusinessDayYmd(day_ymd: number, days_after: number): number;
 export declare function getPreviousKoreanBusinessDayYmd(day_ymd: number, days_before: number): number;
 export declare function getNextKoreanBusinessDayYmdByUtcDate(date: Date, days_after: number): number;
