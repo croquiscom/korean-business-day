6b5402606e3a1f53cebd6b499e4797ddc138dece
Jinho Hyeon <neverlish@gmail.com>
2020-07-13 18:01:23 +0900

    2021년 공휴일 추가

---
+++ src/year_holidays/2021.ts
@@ -0,0 +1,49 @@
+import { IYearHolidays } from '.';
+
+const HOLIDAYS_2020: IYearHolidays = {
+  1: {
+    1: true, // 신정
+  },
+  2: {
+    11: true, // 설날 연휴
+    12: true, // 설날
+    13: true, // 설날 연휴
+  },
+  3: {
+    1: true, // 삼일절
+  },
+  4: {
+
+  },
+  5: {
+    1: true, // 근로자의 날
+    5: true, // 어린이날
+    19: true, // 부처님 오신날
+  },
+  6: {
+    6: true, // 현충일
+  },
+  7: {
+
+  },
+  8: {
+    15: true, // 광복절
+  },
+  9: {
+    20: true, // 추석 연휴
+    21: true, // 추석 연휴
+    22: true, // 추석 연휴
+  },
+  10: {
+    3: true, // 개천절
+    9: true, // 한글날
+  },
+  11: {
+
+  },
+  12: {
+    25: true, // 성탄절
+  },
+};
+
+export default HOLIDAYS_2020;
--- src/year_holidays/index.ts
+++ src/year_holidays/index.ts
@@ -1,5 +1,6 @@
 import HOLIDAYS_2019 from './2019';
 import HOLIDAYS_2020 from './2020';
+import HOLIDAYS_2021 from './2021';
 
 interface IYearHolidays {
   [month: number]: { [day: number]: boolean };
@@ -9,4 +10,5 @@ export {
   IYearHolidays,
   HOLIDAYS_2019,
   HOLIDAYS_2020,
+  HOLIDAYS_2021,
 };
