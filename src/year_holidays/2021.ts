f846b8faf561a0c76507f92cad199e8764606f53
Jinho Hyeon <neverlish@gmail.com>
2020-08-03 11:21:20 +0900

    20200817 / 2021년 공휴일 추가 (#49)

--- src/year_holidays/2021.ts
+++ src/year_holidays/2021.ts
@@ -3,24 +3,22 @@ import { IYearHolidays } from '.';
 const HOLIDAYS_2020: IYearHolidays = {
   1: {
     1: true, // 신정
-    24: true, // 설날 연휴
-    25: true, // 설날
-    26: true, // 설날 연휴
-    27: true, // 대체 휴일
   },
   2: {
-
+    11: true, // 설날 연휴
+    12: true, // 설날
+    13: true, // 설날 연휴
   },
   3: {
     1: true, // 삼일절
   },
   4: {
-    15: true, // 21대 국회의원 선거일
-    30: true, // 부처님 오신날
+
   },
   5: {
     1: true, // 근로자의 날
     5: true, // 어린이날
+    19: true, // 부처님 오신날
   },
   6: {
     6: true, // 현충일
@@ -30,18 +28,18 @@ const HOLIDAYS_2020: IYearHolidays = {
   },
   8: {
     15: true, // 광복절
-    17: true, // 광복절 대체 임시공휴일
   },
   9: {
-    30: true, // 추석 연휴
+    20: true, // 추석 연휴
+    21: true, // 추석 연휴
+    22: true, // 추석 연휴
   },
   10: {
-    1: true, // 추석
-    2: true, // 추석 연휴
     3: true, // 개천절
     9: true, // 한글날
   },
   11: {
+
   },
   12: {
     25: true, // 성탄절
