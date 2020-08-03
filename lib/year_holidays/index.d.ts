16fc254439d2ce2f86b2a6edb9d7544ad7de270d
Jinho Hyeon <neverlish@gmail.com>
2020-08-03 16:31:28 +0900

    v0.3.4 빌드 결과물 생성 (#57)

--- lib/year_holidays/index.d.ts
+++ lib/year_holidays/index.d.ts
@@ -1,8 +1,9 @@
 import HOLIDAYS_2019 from './2019';
 import HOLIDAYS_2020 from './2020';
+import HOLIDAYS_2021 from './2021';
 interface IYearHolidays {
     [month: number]: {
         [day: number]: boolean;
     };
 }
-export { IYearHolidays, HOLIDAYS_2019, HOLIDAYS_2020, };
+export { IYearHolidays, HOLIDAYS_2019, HOLIDAYS_2020, HOLIDAYS_2021, };
