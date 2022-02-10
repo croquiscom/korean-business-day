b470f640f2b8c0ef26d5549bb343d6c99bddb24c
Sangmin Yoon <sixmen@gmail.com>
2022-02-10 09:03:34 +0900

    chore: alphabetize import/order

--- src/year_holidays/index.ts
+++ src/year_holidays/index.ts
@@ -1,8 +1,8 @@
-import { IYearHolidays } from './types';
 import { HOLIDAYS_2019 } from './2019';
 import { HOLIDAYS_2020 } from './2020';
 import { HOLIDAYS_2021 } from './2021';
 import { HOLIDAYS_2022 } from './2022';
+import { IYearHolidays } from './types';
 
 // 토, 일요일 정보는 포함하지 않음
 export const KOREAN_HOLIDAYS: { [year: number]: IYearHolidays } = {
